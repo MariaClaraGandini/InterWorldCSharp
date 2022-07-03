836ed50f67e3b891ce96f497bf9108ca59b0e4d9
JeniferMorati <jenifermorati26@gmail.com>
2022-07-02 23:40:20 -0300

    feat: create budgets page and models

---
+++ InterWorldCSharp/Models/BudgetsDestinationModel.cs
@@ -0,0 +1,31 @@
+﻿namespace InterWorldCSharp.Models
+{
+    public class BudgetsDestinationModel
+    {
+        private string country;
+        private string city;
+        private string img;
+
+        public BudgetsDestinationModel(string country, string city, string img)
+        {
+            this.country = country;
+            this.city = city;
+            this.img = img;
+        }
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+        public string Img
+        {
+            get { return img; }
+            set { img = value; }
+        }
+    }
+}
